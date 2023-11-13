a1b92fef44c7d57ec198a8ff5f2736adabdcdb10
Caio César <caio.c3sar@outlook.com>
2023-11-13 17:02:39 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.png'
+import { SiIfood } from "react-icons/si";
+import { RiWhatsappFill } from 'react-icons/ri'
+import { FaLocationDot } from 'react-icons/fa6'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className='h-screen flex flex-col items-center justify-center'>
+      <div className={`bg-[#ffe6e6ff] w-full flex items-center justify-center p-3 mb-4`}>
+        <img className='h-32' src={logo} alt="" />
+      </div>
+      <div className='mx-auto w-full md:max-w-5xl flex flex-col items-center justify-center mb-4'>
+        <h1 className="font-bold text-2xl">Seja bem vindo</h1>
+        <p>👇 Clique em uma das opções abaixo 👇</p>
+      </div>
+
+      <div className='w-full md:max-w-5xl flex flex-col items-center md:mx-auto justify-center gap-3'>
+        <a className='md:w-7/12 w-11/12'
+          href={`https://api.whatsapp.com/send?phone=5537999162654&text=Olá, estou interessado em conhecer seus lanches, pode me passar seu cardápio por favor?`} target='_blank'>
+          <section className={`w-full h-10 rounded-lg bg-[#bb6e7fff] flex gap-2 items-center justify-center`}>
+            <RiWhatsappFill size={20} color="#000" />
+            <p className='font-medium'>Whatsapp</p>
+          </section>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a className='md:w-7/12 w-11/12'
+          href={`https://www.ifood.com.br/delivery/ok-ok/ok/51fcb63d-ebee-4516-8a61-4b27acb18f79?UTM_Medium=share`} target='_blank'>
+          <section className={`w-full h-10 rounded-lg bg-[#bb6e7fff] flex gap-2 items-center justify-center`}>
+            <SiIfood size={20} color="#000" />
+            <p className='font-medium'>Ifood</p>
+          </section>
+        </a>
+        <a className='md:w-7/12 w-11/12'
+          href={`https://maps.app.goo.gl/X3y97HSGE2WEp6U47`} target='_blank'>
+          <section className={`w-full h-10 rounded-lg bg-[#bb6e7fff] flex gap-2 items-center justify-center`}>
+            <FaLocationDot size={20} color="#000" />
+            <p className='font-medium'>Endereço</p>
+          </section>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
